dc4c56277d2e5e787f661703d92e73a0eb750a4c
The kebab manning <itzsopzzz@gmail.com>
2025-02-01 03:05:53 +0200

    awdfadfq

--- uilibnew.lua
+++ uilibnew.lua
@@ -144,149 +144,149 @@ new(0, 4)
     local TabLibrary = {
         IsFirst = true,
         CurrentTab = ""
-function library:Menu(key)
-    for _,v in next, CoreGuiService:GetChildren() do
-        if v.Name == "screen" then
-            v:Destroy()
-        end
-    end
+        function library:Menu(key)
+            for _,v in next, CoreGuiService:GetChildren() do
+                if v.Name == "screen" then
+                    v:Destroy()
+                end
+            end
 
-    local title = library.title
-    key = key or Enum.KeyCode.Insert or library.key
+            local title = library.title
+            key = key or Enum.KeyCode.Insert or library.key
 
-    local screen = Instance.new("ScreenGui")
-    local background = Instance.new("Frame")
-    local backgroundCorner = Instance.new("UICorner")
-    local backgroundStroke = Instance.new("UIStroke")
-    local headerText = Instance.new("TextLabel")
-    local headerPadding = Instance.new("UIPadding")
-    local tabsFrame = Instance.new("ScrollingFrame")
-
-
-
-    local barFolder = Instance.new("Folder")
-    local bar = Instance.new("Frame")
-    local barGradient = Instance.new("UIGradient")
-    local barLayout = Instance.new("UIListLayout")
-    
-    local tabButtonsGradient = Instance.new("UIGradient")
-    local tabButtonLayout = Instance.new("UIListLayout")
-    local tabButtonPadding = Instance.new("UIPadding")
-    local containerEdge = Instance.new("Frame")
-    local container = Instance.new("Frame")
-    local containerGradient = Instance.new("UIGradient")
-
-    screen.Name = "screen"
-    screen.Parent = CoreGuiService
-    screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-
-    drag(background, 0.04)
-    local CanChangeVisibility = true
-    UserInputService.InputBegan:Connect(function(input)
-        if CanChangeVisibility and input.KeyCode == key then
-            background.Visible = not background.Visible
-        end
-    end)
-
-    background.Name = "background"
-    background.Parent = screen
-    background.AnchorPoint = Vector2.new(0.5, 0.5)
-    background.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    background.Position = UDim2.new(0.5, 0, 0.5, 0)
-    background.Size = UDim2.new(0, 590, 0, 402)
-    background.ClipsDescendants = true
-
-    backgroundCorner.CornerRadius = UDim.new(0, 20  )
-    backgroundCorner.Name = "backgroundCorner"
-    backgroundCorner.Parent = background
-
-    backgroundStroke.Name = "backgroundStroke"
-    backgroundStroke.Parent = background
-    backgroundStroke.Thickness = 1.1
-    backgroundStroke.Color = Color3.fromRGB(0, 76, 255)
-
-    headerText.Name = "headerText"
-    headerText.Parent = background
-    headerText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    headerText.BackgroundTransparency = 1.000
-    headerText.Size = UDim2.new(0, 675, 0, 39)
-    headerText.Font = Enum.Font.Code
-    headerText.Text = title
-    headerText.TextColor3 = Color3.fromRGB(129, 129, 129)
-    headerText.TextSize = 39.000
-    headerText.TextXAlignment = Enum.TextXAlignment.Left
-    headerText.RichText = true
-
-    tabsFrame.Name = "tabsFrame"
-    tabsFrame.Parent = background
-    tabsFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-    tabsFrame.BackgroundColor3 = Color3.fromRGB(235, 235, 235)
-    tabsFrame.ClipsDescendants = true
-    tabsFrame.Position = UDim2.new(0.013, 0, 0.12, 0)
-    tabsFrame.Size = UDim2.new(0, 100, 0, 290)
+            local screen = Instance.new("ScreenGui")
+            local background = Instance.new("Frame")
+            local backgroundCorner = Instance.new("UICorner")
+            local backgroundStroke = Instance.new("UIStroke")
+            local headerText = Instance.new("TextLabel")
+            local headerPadding = Instance.new("UIPadding")
+            local tabsFrame = Instance.new("ScrollingFrame")
 
 
 
+            local barFolder = Instance.new("Folder")
+            local bar = Instance.new("Frame")
+            local barGradient = Instance.new("UIGradient")
+            local barLayout = Instance.new("UIListLayout")
+            
+            local tabButtonsGradient = Instance.new("UIGradient")
+            local tabButtonLayout = Instance.new("UIListLayout")
+            local tabButtonPadding = Instance.new("UIPadding")
+            local containerEdge = Instance.new("Frame")
+            local container = Instance.new("Frame")
+            local containerGradient = Instance.new("UIGradient")
 
-    --[[
-    backgroundGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(34, 34, 34)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(28, 28, 28))}
-    backgroundGradient.Rotation = 90
-    backgroundGradient.Name = "backgroundGradient"
-    backgroundGradient.Parent = background
-
-    headerLabel.Name = "headerLabel"
-    headerLabel.Parent = background
-    headerLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    headerLabel.BackgroundTransparency = 1.000
-    headerLabel.Size = UDim2.new(0, 592, 0, 38)
-    headerLabel.Font = Enum.Font.Code
-    headerLabel.Text = title
-    headerLabel.TextColor3 = Color3.fromRGB(198, 198, 198)
-    headerLabel.TextSize = 16.000
-    headerLabel.TextXAlignment = Enum.TextXAlignment.Left
-    headerLabel.RichText = true
-
-    headerPadding.Name = "headerPadding"
-    headerPadding.Parent = headerLabel
-    headerPadding.PaddingBottom = UDim.new(0, 6)
-    headerPadding.PaddingLeft = UDim.new(0, 12)
-    headerPadding.PaddingRight = UDim.new(0, 6)
-    headerPadding.PaddingTop = UDim.new(0, 6)
-
-    barFolder.Name = "barFolder"
-    barFolder.Parent = background
-
-    bar.Name = "bar"
-    bar.Parent = barFolder
-    bar.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    bar.BackgroundTransparency = 0.200
-    bar.Size = UDim2.new(0, 592, 0, 1)
-    bar.BorderSizePixel = 0
-    
-    barGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(61, 135, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(128, 94, 208))}
-    barGradient.Rotation = 0
-    barGradient.Name = "barGradient"
-    barGradient.Parent = bar
-
-    barLayout.Name = "barLayout"
-    barLayout.Parent = barFolder
-    barLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
-    barLayout.SortOrder = Enum.SortOrder.LayoutOrder
+            screen.Name = "screen"
+            screen.Parent = CoreGuiService
+            screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 
-    
+            drag(background, 0.04)
+            local CanChangeVisibility = true
+            UserInputService.InputBegan:Connect(function(input)
+                if CanChangeVisibility and input.KeyCode == key then
+                    background.Visible = not background.Visible
+                end
+            end)
 
-    tabButtonLayout.Name = "tabButtonLayout"
-    tabButtonLayout.Parent = tabButtons
-    tabButtonLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
-    tabButtonLayout.SortOrder = Enum.SortOrder.LayoutOrder
+            background.Name = "background"
+            background.Parent = screen
+            background.AnchorPoint = Vector2.new(0.5, 0.5)
+            background.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            background.Position = UDim2.new(0.5, 0, 0.5, 0)
+            background.Size = UDim2.new(0, 590, 0, 402)
+            background.ClipsDescendants = true
 
-    tabButtonPadding.Name = "tabButtonPadding"
-    tabButtonPadding.Parent = tabButtons
-    tabButtonPadding.PaddingBottom = UDim.new(0, 4)
-    tabButtonPadding.PaddingLeft = UDim.]]--
-    }
+            backgroundCorner.CornerRadius = UDim.new(0, 20  )
+            backgroundCorner.Name = "backgroundCorner"
+            backgroundCorner.Parent = background
+
+            backgroundStroke.Name = "backgroundStroke"
+            backgroundStroke.Parent = background
+            backgroundStroke.Thickness = 1.1
+            backgroundStroke.Color = Color3.fromRGB(0, 76, 255)
+
+            headerText.Name = "headerText"
+            headerText.Parent = background
+            headerText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            headerText.BackgroundTransparency = 1.000
+            headerText.Size = UDim2.new(0, 675, 0, 39)
+            headerText.Font = Enum.Font.Code
+            headerText.Text = title
+            headerText.TextColor3 = Color3.fromRGB(129, 129, 129)
+            headerText.TextSize = 39.000
+            headerText.TextXAlignment = Enum.TextXAlignment.Left
+            headerText.RichText = true
+
+            tabsFrame.Name = "tabsFrame"
+            tabsFrame.Parent = background
+            tabsFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+            tabsFrame.BackgroundColor3 = Color3.fromRGB(235, 235, 235)
+            tabsFrame.ClipsDescendants = true
+            tabsFrame.Position = UDim2.new(0.013, 0, 0.12, 0)
+            tabsFrame.Size = UDim2.new(0, 100, 0, 290)
+
+
+
+
+            --[[
+            backgroundGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(34, 34, 34)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(28, 28, 28))}
+            backgroundGradient.Rotation = 90
+            backgroundGradient.Name = "backgroundGradient"
+            backgroundGradient.Parent = background
+
+            headerLabel.Name = "headerLabel"
+            headerLabel.Parent = background
+            headerLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            headerLabel.BackgroundTransparency = 1.000
+            headerLabel.Size = UDim2.new(0, 592, 0, 38)
+            headerLabel.Font = Enum.Font.Code
+            headerLabel.Text = title
+            headerLabel.TextColor3 = Color3.fromRGB(198, 198, 198)
+            headerLabel.TextSize = 16.000
+            headerLabel.TextXAlignment = Enum.TextXAlignment.Left
+            headerLabel.RichText = true
+
+            headerPadding.Name = "headerPadding"
+            headerPadding.Parent = headerLabel
+            headerPadding.PaddingBottom = UDim.new(0, 6)
+            headerPadding.PaddingLeft = UDim.new(0, 12)
+            headerPadding.PaddingRight = UDim.new(0, 6)
+            headerPadding.PaddingTop = UDim.new(0, 6)
+
+            barFolder.Name = "barFolder"
+            barFolder.Parent = background
+
+            bar.Name = "bar"
+            bar.Parent = barFolder
+            bar.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            bar.BackgroundTransparency = 0.200
+            bar.Size = UDim2.new(0, 592, 0, 1)
+            bar.BorderSizePixel = 0
+            
+            barGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(61, 135, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(128, 94, 208))}
+            barGradient.Rotation = 0
+            barGradient.Name = "barGradient"
+            barGradient.Parent = bar
+
+            barLayout.Name = "barLayout"
+            barLayout.Parent = barFolder
+            barLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+            barLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+
+            
+
+            tabButtonLayout.Name = "tabButtonLayout"
+            tabButtonLayout.Parent = tabButtons
+            tabButtonLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+            tabButtonLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+            tabButtonPadding.Name = "tabButtonPadding"
+            tabButtonPadding.Parent = tabButtons
+            tabButtonPadding.PaddingBottom = UDim.new(0, 4)
+            tabButtonPadding.PaddingLeft = UDim.]]--
+        }
     CreateTween("tab_text_colour", 0.16)
     function TabLibrary:Menu()
         title = title or "tab"
